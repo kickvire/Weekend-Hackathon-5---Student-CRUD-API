610e068daa2b58d434fc0982734811b299385676
Varun kumar <45607763+kickvire@users.noreply.github.com>
2020-11-15 17:33:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,50 @@
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
-const studentArray = require('./InitialData');
+//const studentArray = require('./InitialData');
+var studentArray = [{
+    id: 1,
+    name: 'Jayesh',
+    currentClass: 5,
+    division: 'A'
+},
+{
+    id: 2,
+    name: 'Minakshi',
+    currentClass: 12,
+    division: 'C'
+},
+{
+    id: 3,
+    name: 'Drisham',
+    currentClass: 7,
+    division: 'C'
+},
+{
+    id: 4,
+    name: 'Kamlesh',
+    currentClass: 7,
+    division: 'A'
+},
+{
+    id: 5,
+    name: 'Dhoni',
+    currentClass: 10,
+    division: 'D'
+},
+{
+    id: 6,
+    name: 'Piyush',
+    currentClass: 10,
+    division: 'A'
+},
+{
+    id: 7,
+    name: 'Aansh',
+    currentClass: 8,
+    division: 'A'
+}]
+
 const port = 8080
 app.use(express.urlencoded());
 
@@ -15,10 +58,10 @@ app.get('/api/student',(req,res)=>{
 })
 app.get('/api/student/:id',(req,res)=>{
     const id=req.params.id;
-
     const student=studentArray.find(student=>student.id===parseInt(id));
     if(!student){
         res.status(404);
+        return;
     }
     res.send(student);
 })
