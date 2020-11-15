65a81738ba995049564aafc498170f548872a0c2
Varun kumar <45607763+kickvire@users.noreply.github.com>
2020-11-15 17:11:05 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
+const studentArray = require('./InitialData');
 const port = 8080
 app.use(express.urlencoded());
 
@@ -9,7 +10,18 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // your code goes here
+app.get('/api/student',(req,res)=>{
+    res.send(studentArray);
+})
+app.get('/api/student/:id',(req,res)=>{
+    const id=req.params.id;
 
+    const student=studentArray.find(student=>student.id===parseInt(id));
+    if(!student){
+        res.status(404);
+    }
+    res.send(student);
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
