2b73d24bc74e2cd2cdd3326984fcfab4a8be9cd8
Varun kumar <45607763+kickvire@users.noreply.github.com>
2020-11-16 23:34:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,71 +1,70 @@
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
-//const studentArray = require('./InitialData');
-var studentArray = [{
-    id: 1,
-    name: 'Jayesh',
-    currentClass: 5,
-    division: 'A'
-},
-{
-    id: 2,
-    name: 'Minakshi',
-    currentClass: 12,
-    division: 'C'
-},
-{
-    id: 3,
-    name: 'Drisham',
-    currentClass: 7,
-    division: 'C'
-},
-{
-    id: 4,
-    name: 'Kamlesh',
-    currentClass: 7,
-    division: 'A'
-},
-{
-    id: 5,
-    name: 'Dhoni',
-    currentClass: 10,
-    division: 'D'
-},
-{
-    id: 6,
-    name: 'Piyush',
-    currentClass: 10,
-    division: 'A'
-},
-{
-    id: 7,
-    name: 'Aansh',
-    currentClass: 8,
-    division: 'A'
-}]
-
 const port = 8080
 app.use(express.urlencoded());
 
+const data = require('./InitialData.js');
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // your code goes here
-app.get('/api/student',(req,res)=>{
-    res.send(studentArray);
-})
-app.get('/api/student/:id',(req,res)=>{
-    const id=req.params.id;
-    const student=studentArray.find(student=>student.id===parseInt(id));
-    if(!student){
+
+app.get('/api/student',(req,res) => {
+    res.send(data);
+});
+
+app.get('/api/student/:id',(req,res) => {
+    const id = req.params.id;
+    const student = data.find((student) => student.id === parseInt(id));
+    if(!student) {
         res.status(404);
         return;
     }
     res.send(student);
-})
 
+});
+
+app.post('/api/student',(req,res) => {
+    const {name, currentClass, division} = req.body;
+    if((!name) || (!currentClass) || (!division)) {
+        res.status(400);
+        return;
+    }
+    const newStudent = {
+        id: data.length + 1,
+        name: name,
+        currentClass: currentClass,
+        division: division
+    }
+    data.push(newStudent);
+    res.send(newStudent.id);
+
+});
+
+app.put('/api/student/:id',(req,res) => {
+    const id = req.params.id;
+    const student = data.find(student => student.id === parseInt(id));
+    const newName = req.body.name;
+    if((!student) || (!newName)) { 
+        res.status(400);
+        return;
+    }
+    student.name = newName;
+
+});
+
+app.delete('/api/student/:id',(req,res) => {
+    const id = req.params.id;
+    const studentIndex = data.findIndex((student) => parseInt(id) === student.id);
+    if(studentIndex === -1) {
+        res.status(400);
+        return;
+    }
+    
+    data.splice(studentIndex,1);
+})
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
 module.exports = app;   
